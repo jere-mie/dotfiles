a28d1a19c078449c16907418df2fc7d6c603994c
Jeremie Bornais <jeremiejbornais@gmail.com>
2022-03-05 14:25:20 -0500

    added replit nix config

---
+++ replit.nix
@@ -0,0 +1,14 @@
+{ pkgs }: {
+    deps = [
+        pkgs.jdk11
+        pkgs.tmux
+        pkgs.wget
+        pkgs.python3
+        pkgs.nodejs
+        pkgs.neofetch
+        pkgs.vim
+        pkgs.neovim
+        pkgs.man
+        pkgs.wine
+    ];
+}
