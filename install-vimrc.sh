37ff1d6d1d8e263069ae3923004a23158415d7e1
Jeremie Bornais <jeremiejbornais@gmail.com>
2022-03-06 16:22:53 -0500

    updated vimrc install

--- install-vimrc.sh
+++ install-vimrc.sh
@@ -2,4 +2,3 @@ git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime # download 
 sh ~/.vim_runtime/install_awesome_vimrc.sh # install the ultimate vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim # download vim plug
 curl -fLo ~/.vim_runtime/my_configs.vim --create-dirs https://raw.githubusercontent.com/jere-mie/dotfiles/main/my_configs.vim # download my custom vimrc
-vim -c PlugInstall
